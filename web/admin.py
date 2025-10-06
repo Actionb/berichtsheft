2abc10cfaad1bfe21cc1215c9955d994a617a7fe
Actionb <yummytea1@gmail.com>
2025-10-06 15:34:53 +0200

    Add ModelAdmins for Nachweis and Abteilung models

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from web import models as _models
+
+
+@admin.register(_models.Nachweis)
+class NachweisAdmin(admin.ModelAdmin):
+    list_display = [
+        "jahr",
+        "ausbildungswoche",
+        "datum_start",
+        "datum_end",
+        "abteilung",
+        "fertig",
+        "eingereicht_bei",
+        "unterschrieben",
+    ]
+    autocomplete_fields = ["abteilung"]
+
+
+@admin.register(_models.Abteilung)
+class AbteilungAdmin(admin.ModelAdmin):
+    search_fields = ["name"]

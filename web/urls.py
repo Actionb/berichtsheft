c43fe080af8e1d27042566fb94343b8cdba385e5
Actionb <yummytea1@gmail.com>
2025-10-21 10:21:14 +0200

    Add Abteilung autocomplete dropdown

--- web/urls.py
+++ web/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from web.views import NachweisEditView, NachweisListView, NachweisPrintView
+from web.views import AbteilungEditView, AutocompleteView, NachweisEditView, NachweisListView, NachweisPrintView
 
 urlpatterns = [
-    path("add/", NachweisEditView.as_view(extra_context={"add": True}), name="nachweis_add"),
-    path("<path:pk>/change/", NachweisEditView.as_view(extra_context={"add": False}), name="nachweis_change"),
-    path("<path:pk>/print/", NachweisPrintView.as_view(), name="nachweis_print"),
-    path("", NachweisListView.as_view(), name="nachweis_list"),
+    path("nachweis/add/", NachweisEditView.as_view(extra_context={"add": True}), name="nachweis_add"),
+    path("nachweis/<path:pk>/change/", NachweisEditView.as_view(extra_context={"add": False}), name="nachweis_change"),
+    path("nachweis/<path:pk>/print/", NachweisPrintView.as_view(), name="nachweis_print"),
+    path("nachweis/", NachweisListView.as_view(), name="nachweis_list"),
+    path("abteilung/add/", AbteilungEditView.as_view(extra_context={"add": True}), name="abteilung_add"),
+    path(
+        "abteilung/<path:pk>/change/", AbteilungEditView.as_view(extra_context={"add": False}), name="abteilung_change"
+    ),
+    path("abteilung_ac/", AutocompleteView.as_view(), name="abteilung_ac"),
 ]
--- web/views.py
+++ web/views.py
@@ -1,8 +1,16 @@
 from django import forms
 from django.urls import reverse_lazy
 from django.views.generic import DetailView, ListView, UpdateView
+from mizdb_tomselect.views import AutocompleteView as BaseAutocompleteView
+from mizdb_tomselect.views import PopupResponseMixin
+from mizdb_tomselect.widgets import MIZSelect
 
-from web.models import Nachweis
+from web.models import Abteilung, Nachweis
+
+
+class AutocompleteView(BaseAutocompleteView):
+    def has_add_permission(self, request):
+        return True
 
 
 class BaseViewMixin:
@@ -14,28 +22,49 @@ class BaseViewMixin:
         return context
 
 
-class NachweisEditView(BaseViewMixin, UpdateView):
-    model = Nachweis
-    template_name = "nachweis_edit.html"
-    fields = forms.ALL_FIELDS
-    title = "Nachweis Bearbeiten"
-    success_url = reverse_lazy("nachweis_list")
+class ModelViewMixin:
+    model = None
 
+    def __init__(self, *args, **kwargs):
+        self.opts = self.model._meta
+        super().__init__(*args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(opts=self.opts, **kwargs)
+
+
+class EditView(ModelViewMixin, BaseViewMixin, UpdateView):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.add = self.extra_context["add"]
+        if not self.title:
+            self.title = f"{self.opts.verbose_name} {'erstellen' if self.add else 'bearbeiten'}"
 
     def get_object(self, queryset=None):
         if not self.add:
             return super().get_object(queryset)
+
+
+class NachweisEditView(EditView):
+    model = Nachweis
+    template_name = "nachweis_edit.html"
+    fields = forms.ALL_FIELDS
+    success_url = reverse_lazy("nachweis_list")
 
     def get_form_class(self):
         return forms.modelform_factory(
             Nachweis,
             fields=self.fields,
             widgets={
-                "datum_start": forms.DateInput(attrs={"type": "date"}),
-                "datum_ende": forms.DateInput(attrs={"type": "date"}),
+                "datum_start": forms.DateInput(attrs={"type": "date"}, format="%Y-%m-%d"),
+                "datum_ende": forms.DateInput(attrs={"type": "date"}, format="%Y-%m-%d"),
+                "abteilung": MIZSelect(
+                    Abteilung,
+                    url="abteilung_ac",
+                    add_url="abteilung_add",
+                    edit_url="abteilung_change",
+                    create_field="name",
+                ),
             },
         )
 
@@ -49,3 +78,10 @@ class NachweisListView(BaseViewMixin, ListView):
 class NachweisPrintView(BaseViewMixin, DetailView):
     model = Nachweis
     template_name = "print.html"
+
+
+class AbteilungEditView(PopupResponseMixin, EditView):
+    model = Abteilung
+    template_name = "base_form.html"
+    fields = forms.ALL_FIELDS
+    success_url = reverse_lazy("nachweis_list")

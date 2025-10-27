b358ea318dbd29043bc99e167f01bfb85603de37
Actionb <yummytea1@gmail.com>
2025-10-27 10:02:48 +0100

    Add permission fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,13 @@ import contextlib
 from unittest import mock
 
 import pytest
+from django.contrib.auth import get_permission_codename
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
+
+################################################################################
+# MOCKS
+################################################################################
 
 
 @pytest.fixture
@@ -30,5 +37,55 @@ def mock_super_method():
             return_mock = super_mock.return_value
             return_mock.attach_mock(mock.Mock(return_value=ret), attr)
             yield
+
+    return inner
+
+
+################################################################################
+# USER & PERMISSIONS
+################################################################################
+
+
+@pytest.fixture
+def username():
+    """Default username for tests. Overwrite via test parametrization."""
+    return "testuser"
+
+
+@pytest.fixture
+def user(django_user_model, username):
+    """Create a user for testing."""
+    return django_user_model.objects.create(username=username)
+
+
+@pytest.fixture
+def login_user(client, user):
+    """Login the test user."""
+    client.force_login(user)
+
+
+@pytest.fixture
+def add_permission(reload_user):
+    """Add a permission to the test user."""
+
+    def inner(user, action, opts, reload=True):
+        perm, _ = Permission.objects.get_or_create(
+            codename=get_permission_codename(action, opts),
+            content_type=ContentType.objects.get_for_model(opts.model),
+        )
+        user.user_permissions.add(perm)
+        if reload:
+            return reload_user(user)
+        return user
+
+    return inner
+
+
+@pytest.fixture
+def reload_user(django_user_model):
+    """Reload user from database and return it. This resets the permission cache."""
+
+    def inner(user):
+        return django_user_model.objects.get(pk=user.pk)
 
     return inner

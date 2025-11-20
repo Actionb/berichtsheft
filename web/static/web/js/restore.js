606ba84318492f32378178507cfb8cdb1ba86d76
Actionb <yummytea1@gmail.com>
2025-11-20 09:15:34 +0100

    Add restore.js script

---
+++ web/static/web/js/restore.js
@@ -0,0 +1,41 @@
+/**
+ * Restore a model instance from the model's deleted objects.
+ */
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const restoreButtons = document.querySelectorAll(".restore-btn");
+    restoreButtons.forEach(button => {
+        button.addEventListener("click", function (event) {
+            event.preventDefault();
+            const url = button.getAttribute("href");
+            fetch(url, {
+                method: "POST",
+                headers: {
+                    "X-Requested-With": "XMLHttpRequest",
+                    "X-CSRFToken": getCookie("csrftoken")
+                }
+            }).then(response => {
+                if (response.ok) {
+                    location.reload()
+                } else {
+                    alert("Fehler beim Wiederherstellen des Objekts.");
+                }
+            });
+        })
+    })
+})

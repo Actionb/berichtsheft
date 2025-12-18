ca3ad798f492a2ed6e4d9d03874d74165355a1fd
Actionb <yummytea1@gmail.com>
2025-12-18 09:31:30 +0100

    admin: add list filters

--- web/admin.py
+++ web/admin.py
@@ -7,9 +7,28 @@ from web import models as _models
 admin.site.site_url = reverse_lazy("home")
 
 
+class EingereichtFilter(admin.AllValuesFieldListFilter):
+    def choices(self, changelist):
+        # Add an option to filter for Nachweise that have not been submitted to
+        # anybody.
+        display = "Nicht eingereicht"
+        queryset = changelist.get_queryset(self.request, exclude_parameters=self.expected_parameters())
+        if changelist.add_facets:
+            display = f"{display} ({queryset.filter(eingereicht_bei='').count()})"
+        yield {
+            "selected": bool(self.lookup_val == ""),
+            "query_string": changelist.get_query_string({self.lookup_kwarg: ""}),
+            "display": display,
+        }
+        for choice in super().choices(changelist):
+            yield choice
+
+
 @admin.register(_models.Nachweis)
 class NachweisAdmin(admin.ModelAdmin):
     list_display = [
+        "user",
+        "nummer",
         "jahr",
         "ausbildungswoche",
         "datum_start",
@@ -19,6 +38,9 @@ class NachweisAdmin(admin.ModelAdmin):
         "eingereicht_bei",
         "unterschrieben",
     ]
+    list_display_links = ["nummer"]
+    list_editable = ["fertig", "unterschrieben"]
+    list_filter = ["user", "fertig", ("eingereicht_bei", EingereichtFilter), "unterschrieben"]
     exclude = ["deleted_at", "restored_at", "transaction_id"]
     autocomplete_fields = ["abteilung"]
 

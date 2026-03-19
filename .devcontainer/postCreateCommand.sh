fe5f40debb9385a8fe236ee31be1f28c2c8875b6
Actionb <yummytea1@gmail.com>
2026-03-19 07:19:19 +0000

    Allow existing venvs

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -6,7 +6,7 @@ echo "alias uv='uv --native-tls'" >> ~/.bashrc
 
 # Create virtual environment:
 echo "Creating virtual environment..."
-uv venv --clear .venv
+uv venv --allow-existing .venv
 echo 'export VIRTUAL_ENV=/home/vscode/workspace/.venv' >> ~/.bashrc
 echo 'export PATH=$VIRTUAL_ENV/bin:$PATH' >> ~/.bashrc
 
